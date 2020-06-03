47a7bee954bfc99f875801fe5e0ce50b9e4fb178
zanebliss <zanebliss@icloud.com>
2020-06-03 18:14:51 -0500

    testing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import brain from 'brain.js'
 import './App.css';
+import Button from 'react-bootstrap/Button'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Doughnut } from 'react-chartjs-2'
+
+// Data set
+// const arr = [0,0,1,1]
+// const result = [0]
+// New NeuralNetwork
+const net = new brain.NeuralNetwork({ hiddenLayers: [3] });
+let trainingData = [
+  { input: [0,0,1,1], output: [1]}
+]
+// trainingData.push({input: arr, output: result})
+net.train(trainingData)
+let hmm = (net.run([0,0,1,1]))
+let hmm2 = Math.floor(hmm[0] * 100)
+
 
 function App() {
+  const [doughtnutData, setDoughnutData] = useState({})
+
+  const doughnut = () => {
+    setDoughnutData({
+      // labels: ['Sleep Score', ''],
+      datasets: [
+        {
+          label: 'Sleep score',
+          data: [hmm2, 100 - hmm2],
+          backgroundColor: ['#56CCF2', 'transparent']
+        }
+      ],
+    })
+  }
+  const options = {
+    tooltips: {
+      enabled: false
+    }
+  }
+
+  useEffect(() => {
+    doughnut()
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className='container'>
+        <div className='number'>
+          <p id='number'>{hmm2}</p>
+        <Doughnut data={doughtnutData} options={options} className='doughnut'/>
+        </div>
+        <div className='test'>
+          <Button variant='primary' size='lg' className='shadow'>Test</Button>{' '}
+        </div>
+      </div>
+    </>
   );
 }
 
