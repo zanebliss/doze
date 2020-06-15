63accb6de0aca12ac1beb189f9766b5f38e7f15f
zanebliss <zanebliss@icloud.com>
2020-06-15 09:57:37 -0500

    added labels for navbar items

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -5,23 +5,27 @@ import { House, ClipboardData, LayoutTextSidebarReverse, List } from 'react-boot
 import './Navbar.css'
 
 const Bar = () => {
-    const iconSize = '50'
+    const iconSize = '40'
 
     return (
         <>
             <div className='nav-wrapper'>
                 <Navbar fixed='bottom' className='nav'>
-                    <NavLink exact to='/home' activeStyle={{ color: '#2D9CDB' }} className='icon' >
+                    <NavLink exact to='/home' activeStyle={{ color: '#2D9CDB' }} style={{ textDecoration: 'none'}} className='icon' >
                         <House size={iconSize} />
+                        <div className='label'>Home</div>
                     </NavLink>
-                    <NavLink exact to='/trends' activeStyle={{ color: '#2D9CDB' }} className='icon' >
+                    <NavLink exact to='/trends' activeStyle={{ color: '#2D9CDB' }} style={{ textDecoration: 'none'}} className='icon' >
                         <ClipboardData size={iconSize} />
+                        <div className='label'>Trends</div>
                     </NavLink>
-                    <NavLink exact to='/entries' activeStyle={{ color: '#2D9CDB' }} className='icon' >
-                        <LayoutTextSidebarReverse size={47} />
+                    <NavLink exact to='/entries' activeStyle={{ color: '#2D9CDB' }} style={{ textDecoration: 'none'}} className='icon' >
+                        <LayoutTextSidebarReverse size={iconSize} />
+                        <div className='label'>Journal</div>
                     </NavLink>
-                    <NavLink exact to='/settings' activeStyle={{ color: '#2D9CDB' }} className='icon'>
+                    <NavLink exact to='/settings' activeStyle={{ color: '#2D9CDB' }} style={{ textDecoration: 'none'}} className='icon'>
                         <List size={iconSize} />
+                        <div className='label'>Settings</div>
                     </NavLink>
 
                 </Navbar>
