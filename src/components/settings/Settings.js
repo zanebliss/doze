084a7b7d7fd8066184302d4c0050998b8edac1e9
zanebliss <zanebliss@icloud.com>
2020-06-15 09:47:28 -0500

    settings wip

--- src/components/settings/Settings.js
+++ src/components/settings/Settings.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import './Settings.css'
 
 const Settings = () => {
 
     return (
         <>
+            <div className='settings-wrapper'>
+                <div className='header'>
+                    <div className='header-text'>Settings</div>
+                </div>
+            </div>
         </>
     )
 }
